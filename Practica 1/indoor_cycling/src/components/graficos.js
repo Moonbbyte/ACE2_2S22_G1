2ae664d954ba62a7d731f9910035e1754dac8fe5
Moonbbyte <63923585+Moonbbyte@users.noreply.github.com>
2022-08-15 12:51:51 -0600

    pruebachartj1

--- Practica 1/indoor_cycling/src/components/graficos.js
+++ Practica 1/indoor_cycling/src/components/graficos.js
@@ -1,12 +1,71 @@
 import { Component } from "react"
 import React from 'react';
+import {Bar,Line} from 'react-chartjs-2';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+  
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+  export const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Bar Chart',
+      },
+    },
+  };
+  
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  
+  export const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: [1,2,3,4,5,6,7],
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'Dataset 2',
+        data: [1,2,3,4,5,6,7],
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
+
+
+  
 
 
 export default class Graficos extends Component{
     render(){
-        return  <div className="col-8 container" id="Graphic-Component" align="center">
-                    <img width="100%" height="100%" src={require("../images/ciclismoWall.jpg")}/>
+        return (
+            <div className="col-8 container" id="Graphic-Component" align="center">
+                <div>
+                 <Bar options={options} data={data} />;
                 </div>
+                
+               
+                    
+            </div>
+        ); 
     }
 
 }
