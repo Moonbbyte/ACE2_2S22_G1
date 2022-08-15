cfcbca51a39f7795e9e49d502d7af41008c7b13c
Moonbbyte <63923585+Moonbbyte@users.noreply.github.com>
2022-08-15 11:36:34 -0600

    f1

--- Practica 1/indoor_cycling/src/components/Experimentos.js
+++ Practica 1/indoor_cycling/src/components/Experimentos.js
@@ -5,9 +5,9 @@ import React from 'react';
 export default class Experimetos extends Component{
     render(){
         return <div className="btn-group-vertical col-2" id="Exp-Component">
-                    <button className="btnEffect btn btn-secondary">Experimento 1</button>
-                    <button className="btnEffect btn btn-secondary">Experimento 2</button>
-                    <button className="btnEffect btn btn-secondary">Experimento 3</button>
+                    <button className="btnEffect btn btn-dark">Experimento 1</button>
+                    <button className="btnEffect btn btn-dark">Experimento 2</button>
+                    <button className="btnEffect btn btn-dark">Experimento 3</button>
                 </div>
         
         
