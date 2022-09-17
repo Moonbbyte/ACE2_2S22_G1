492383b7bbe706d93d7104a11f4a28b03a9c3017
Vernik22 <3003160030101@ingenieria.usac.edu.gt>
2022-09-17 14:44:39 -0600

    Serial Port en Node.js
    prueba si funciona el serial port en node js

--- Proyecto 1/backJs/src/app.js
+++ Proyecto 1/backJs/src/app.js
@@ -1,11 +1,21 @@
 import express from "express";
 import morgan from "morgan";
+import { SerialPortMock } from 'serialport'
 
 //Routes de Practica1
 import languageRoutes from "./routes/language.routes";
 var cors = require('cors');
 const app = express();
 
+// Import dependencies
+SerialPortMock.binding.createPort('/dev/robot')
+const port = new SerialPortMock({ path: '/dev/robot', baudRate: 9600 })
+
+port.write('Robot power on')
+
+port.on('open', () => {
+  port.port.emitData('data')
+})
 
 //Settings
 app.set("port", 4000);
