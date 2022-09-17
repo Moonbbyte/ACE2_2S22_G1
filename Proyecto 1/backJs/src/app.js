e7bae6cbbc7717781b0807231a9098929f30f5b5
Vernik22 <3003160030101@ingenieria.usac.edu.gt>
2022-09-17 15:01:29 -0600

    .

--- Proyecto 1/backJs/src/app.js
+++ Proyecto 1/backJs/src/app.js
@@ -2,21 +2,20 @@ import express from "express";
 import morgan from "morgan";
 import { SerialPortMock } from 'serialport'
 
+const { SerialPort } = require('serialport');
+const { ReadlineParser } = require('@serialport/parser-readline');
+const port = new SerialPort({ path: '/dev/ROBOT', baudRate: 9600 });
+
+const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
+parser.on('data', console.log);
+
 //Routes de Practica1
 import languageRoutes from "./routes/language.routes";
 var cors = require('cors');
 const app = express();
 
 // Import dependencies
-SerialPortMock.binding.createPort('/dev/robot')
-const port = new SerialPortMock({ path: '/dev/robot', baudRate: 9600 })
 
-port.write('Robot power on')
-
-port.on('open', (lista) => {
-  port.port.emitData('data')
-  console.log(lista);
-})
 
 //Settings
 app.set("port", 4000);
