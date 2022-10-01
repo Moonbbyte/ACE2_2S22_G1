b05e49ea35c45cfca503c5fb17ddc4ee0751c17d
Vernik22 <3003160030101@ingenieria.usac.edu.gt>
2022-10-01 14:09:35 -0600

    ...

--- Practica 2/backJs/src/app.js
+++ Practica 2/backJs/src/app.js
@@ -4,16 +4,6 @@ import SerialPort from "serialport";
 import { methods as practica } from "./controllers/practica2.serialport";
 
 
-let date_ob = new Date();
-
-let date = ("0" + date_ob.getDate()).slice(-2);
-let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-let year = date_ob.getFullYear();
-let hours = date_ob.getHours();
-let minutes = date_ob.getMinutes();
-let seconds = date_ob.getSeconds();
-
-let fechaAct = year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds
 
 // Import dependencies
 const port = new SerialPort('COM3',{baudRate: 9600});
