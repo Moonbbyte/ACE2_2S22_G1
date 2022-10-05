e31e5111864dd159cfa455a821a0c4bbee21c65f
Vernik22 <3003160030101@ingenieria.usac.edu.gt>
2022-10-04 22:08:52 -0600

    Flag Prueba

--- Practica 2/backJs/src/app.js
+++ Practica 2/backJs/src/app.js
@@ -14,7 +14,7 @@ port.pipe(parser);
 
 parser.on('data', (line)=>{
     const words = line.split(',');
-    //console.log(words);
+    console.log(line);
     if (words.length == 4) {
         practica.postearCalorias(line,login);
         practica.postearFrecuenciaRep(line,login);
